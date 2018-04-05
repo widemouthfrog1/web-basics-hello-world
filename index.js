2f3dd2d669120a0bda7afd7ad840634dbafcec4b
Karl Bennett <widemouthfrog1@gmail.com>
2018-04-05 21:55:07 +1200

    Fix random trivia button

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ $(function() {
   // console.log will log a message or object to the browser developer console
   console.log("page loaded...");
 
-  $("selector-goes-here").click(/* function for when the button is clicked goes here */);
+  $("button").click(function(){fetchRandomTriviaQuestion(displayQuestionAndAnswer)});
   /*
    * TODO: You will need to use a css selector to get jQuery to find the button element in the page
    * Then you will need to make a new javascript function to do stuff for when the button
